ba6224dfdc857d39e0aad52ff1ab7ca1fc20a56b
Ricky <Ricky@Hosfelt.io>
2020-05-21 13:29:33 -0400

    rustfmt ran

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,21 @@
 #![feature(type_name_of_val)]
 
 #[allow(unused_macros)]
-// ptype == print type :) 
+// ptype == print type :)
 #[macro_export]
 macro_rules! ptype {
     ($input:expr) => {
         println!("{}", std::any::type_name_of_val($input));
-    }
+    };
 }
 
 #[allow(unused_macros)]
-// rtype == return type :) 
+// rtype == return type :)
 #[macro_export]
 macro_rules! rtype {
     ($input:expr) => {
         std::any::type_name_of_val($input)
-    }
+    };
 }
 
 #[test]
@@ -34,16 +34,15 @@ struct Tester {
 
 #[test]
 fn test_custom_type() {
-    let test_h = Tester{x: 42, y:18.0};
+    let test_h = Tester { x: 42, y: 18.0 };
     let t = rtype!(&test_h);
     ptype!(&t);
     assert_eq!(t, "ttype::Tester");
 }
 
-
 #[test]
 fn test_custom_type_sub() {
-    let test_h = Tester{x: 42, y:18.0};
+    let test_h = Tester { x: 42, y: 18.0 };
     let t = rtype!(&test_h.x);
     ptype!(&test_h.x);
     assert_eq!(t, "u64");
